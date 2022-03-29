d134829d7a4da6abc091a699bd3ca5f21e9b4159
L36951 <80443759+L36951@users.noreply.github.com>
2022-03-29 17:29:21 +0800

    Add files via upload

---
+++ api/route/tokenlist.js
@@ -0,0 +1,34 @@
+const jwt = require('jsonwebtoken')
+
+let tokenlist = []
+
+//Store user token
+const setToken = (token) =>{
+    tokenlist.push(token)
+}
+
+//Filter out user token
+const refreshToken = (token) =>{
+    if(checkToken(token)){
+        tokenlist = tokenlist.filter((currtoken) => currtoken !== token)
+        return true
+    }
+    return false
+}
+
+//Check user token exist or not
+const checkToken = (token) =>{
+    if(tokenlist.includes(token)) return true
+    return false
+}
+
+//Check user token by user id and filter its
+const checkTokenById = (id) =>{
+    tokenlist.map((currtoken) => {
+        jwt.verify(currtoken,process.env.JWT_SECRET,(err,curruser) => {
+            if(curruser.id == id) tokenlist = tokenlist.filter((token) => token !== currtoken)     
+        })
+    })
+}
+
+module.exports = { setToken , refreshToken ,checkToken , checkTokenById }
